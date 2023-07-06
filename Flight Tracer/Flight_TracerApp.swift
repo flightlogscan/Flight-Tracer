be7e73d06385009535dd6147c588d91f8c8d19fa
william janis <wilbur777@protonmail.com>
2023-07-05 17:36:14 -0700

    Initial Commit

---
+++ Flight Tracer/Flight_TracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flight_TracerApp.swift
+//  Flight Tracer
+//
+//  Created by William Janis on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Flight_TracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
