983d408196a6f23d440d5d49a9a2e0b21f57e3d2
lancedesi <lancedesi@msn.com>
2023-09-09 19:27:39 -0700

    Allow multiple images to be uploaded at once

--- Flight Tracer/Views/SubViews/SelectImageView.swift
+++ Flight Tracer/Views/SubViews/SelectImageView.swift
@@ -2,42 +2,56 @@ import SwiftUI
 import _PhotosUI_SwiftUI
 
 struct SelectImageView: View {
-
+    
     @State private var selectedImage: Image?
-    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedItems: [PhotosPickerItem] = []
     @State private var isValidated: Bool = false
     @Binding var isImageValid: Bool
+    @Binding var selectedImages: [ImageDetail]
     @ObservedObject var selectImageViewModel = SelectImageViewModel()
     let recognizedTextProcessor = RecognizedTextProcessor()
     
     var body: some View {
         
-        let photosPicker = PhotosPicker(selection: $selectedItem, matching: .images) {
-            Label("Select a photo", systemImage: "photo")
+        let photosPicker = PhotosPicker(selection: $selectedItems, matching: .images) {
+            Label("Select photos", systemImage: "photo")
         }
-        .tint(.gray.opacity(0.1))
-        .cornerRadius(10)
-        .foregroundColor(Color.black)
-        .buttonStyle(.borderedProminent)
-        .onChange(of: selectedItem) { newItem in
-            Task {
-                if let data = try? await selectedItem?.loadTransferable(type: Data.self) {
-                    if let uiImage = UIImage(data: data) {
-                        selectedImage = Image(uiImage: uiImage)
-                        selectImageViewModel.simpleValidateImage(image: uiImage)
-                        isValidated = true
-                        isImageValid = self.selectImageViewModel.isImageValid
-                        return
+        //.tint(.gray.opacity(0.1))
+            .cornerRadius(10)
+            .foregroundColor(Color.black)
+            .buttonStyle(.borderedProminent)
+            .onChange(of: selectedItems) { newItem in
+                Task {
+                    selectedImages = []
+                    for item in selectedItems {
+                        if let data = try? await item.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data) {
+                                selectedImage = Image(uiImage: uiImage)
+                                let image = Image(uiImage: uiImage)
+                                selectImageViewModel.simpleValidateImage(image: uiImage)
+                                isValidated = true
+                                isImageValid = self.selectImageViewModel.isImageValid
+                                
+                                let imageDetail = ImageDetail(image: image, isValidated: isValidated, isImageValid: isImageValid)
+                                selectedImages.append(imageDetail)
+                            }
+                        }
                     }
                 }
             }
-        }
         
-        if let selectedImage {
-            selectedImage
-                .resizable()
-                .scaledToFit()
-                .clipped()
+        // TODO: All the images are constrained a fixed size, would be cool to enable scrolling through all of them
+        if selectedImages.count > 0 {
+            ForEach(selectedImages) {image in
+                image.image
+                    .resizable()
+                    .scaledToFit()
+                    .clipped()
+                if (image.isValidated && !image.isImageValid) {
+                    Text("Invalid flight log. Please try a new image.")
+                        .foregroundColor(Color.red)
+                }
+            }
             Spacer()
             CameraView()
             photosPicker
@@ -55,4 +69,12 @@ struct SelectImageView: View {
                 .foregroundColor(Color.red)
         }
     }
+}
+
+public struct ImageDetail: Identifiable {
+    public var id = UUID()
+    
+    @State var image: Image
+    @State var isValidated: Bool
+    @State var isImageValid: Bool
 }
