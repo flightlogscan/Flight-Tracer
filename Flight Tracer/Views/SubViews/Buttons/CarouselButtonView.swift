65aee37e28363d6f2dbc554b37ba3d3f6d4052f5
lancedesi <lancedesi@msn.com>
2024-02-03 09:10:18 -0700

    Simple validation of images on carousel select

--- Flight Tracer/Views/SubViews/Buttons/CarouselButtonView.swift
+++ Flight Tracer/Views/SubViews/Buttons/CarouselButtonView.swift
@@ -11,12 +11,18 @@ struct CarouselButtonView: View {
     
     var thumbnailImage: UIImage
     var hiResImage: UIImage
+    @ObservedObject var selectImageViewModel = SelectImageViewModel()
     @Binding var selectedImages: [ImageDetail]
     
     var body: some View {
         Button {
             let image = Image(uiImage: hiResImage)
             let imageDetail = ImageDetail(image: image, uiImage: hiResImage, isValidated: true)
+            
+            // This uses a very basic image scanner as a first-step sanity-check
+            // before allowing users to send the image to the more resource-intensive scanner
+            selectImageViewModel.simpleValidateImage(image: imageDetail)
+            
             selectedImages = []
             selectedImages.append(imageDetail)
             
