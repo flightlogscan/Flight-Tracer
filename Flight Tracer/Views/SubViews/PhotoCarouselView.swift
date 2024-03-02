6b7e7ea0bdb17809ce2e65a533a25b56efac0f83
wilbur <wilbur777@protonmail.com>
2024-03-02 15:44:58 -0700

    Update photo carousel when app is brought into focus

--- Flight Tracer/Views/SubViews/PhotoCarouselView.swift
+++ Flight Tracer/Views/SubViews/PhotoCarouselView.swift
@@ -25,28 +25,40 @@ struct PhotoCarouselView: View {
         .frame(height: 75)
         .padding([.leading, .trailing])
         .onAppear {
-            //This code is gross and needs to be extracted to a different class
-            fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-            fetchOptions.fetchLimit = 10
-            
-            imageRequestOptions.deliveryMode = .highQualityFormat
-            imageRequestOptions.isSynchronous = true
-            
-            let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-            
-            fetchResult.enumerateObjects { (phAsset, _, _) in
-                imageManager.requestImage(
-                    for: phAsset, targetSize: PHImageManagerMaximumSize, contentMode: .default, options: imageRequestOptions
-                ) { (uiImage, _) in
-                    thumbnailImages.append(uiImage!)
-                }
-            }
+            getThumbnailPhotos()
+        }
+        .onReceive(NotificationCenter.default.publisher(
+            for: UIScene.willEnterForegroundNotification)) { _ in
+                // Refresh photo carousel when app is back in focus in case anything changed
+                // Slow, but it works..
+                thumbnailImages = []
+                getThumbnailPhotos()
+                
         }
         .shadow(radius: 1)
     }
     
+    func getThumbnailPhotos() {
+        //This code is gross and needs to be extracted to a different class
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        fetchOptions.fetchLimit = 10
+        
+        imageRequestOptions.deliveryMode = .highQualityFormat
+        imageRequestOptions.isSynchronous = true
+        
+        let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        
+        fetchResult.enumerateObjects { (phAsset, _, _) in
+            imageManager.requestImage(
+                for: phAsset, targetSize: PHImageManagerMaximumSize, contentMode: .default, options: imageRequestOptions
+            ) { (uiImage, _) in
+                thumbnailImages.append(uiImage!)
+            }
+        }
+    }
+    
     // This code is gross and needs to be extracted to a different class
-    // There is a bug where if a photo is added or deleted in the photos app, FLT won't
+    // There is a bug where if a photo is added or deleted in the photos app, FLT won't
     // know to update the index and thus show incorrect images.
     func getImage(index: Int) -> UIImage {
         let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
