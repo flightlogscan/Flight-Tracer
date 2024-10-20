61d5304465093a4a1b2105df25d83efe003a4bfd
lancedesi <lancedesi@msn.com>
2024-10-20 11:02:02 -0700

    Updates

--- Flight Tracer/ViewModel/ContentViewModel.swift
+++ Flight Tracer/ViewModel/ContentViewModel.swift
@@ -6,11 +6,7 @@ class ContentViewModel: ObservableObject {
     
     func processImageText(selectedImage: ImageDetail?, realScan: Bool? = false, user: User?, selectedScanType: Int) {
         if (realScan!) {
-            // Scan the image and get the raw "AnalyzeResult"
             formRecognizer.scanImage(image: selectedImage!, user: user, selectedScanType: selectedScanType)
-            
-            // Convert the raw "AnalyzeResult" to a more usable and sanitized "recognizedText"
-//            selectedImage!.recognizedText = convertTo2DArray(analyzeResult: selectedImage!.analyzeResult!, headers: headers)
         }
     }
 }
