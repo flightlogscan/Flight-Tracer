d6d2405097240939694b4108c2c74d191af5874e
wilbur <wilbur777@protonmail.com>
2024-12-14 09:18:28 -0700

    Fix multi column fields

--- Flight Tracer/Views/SubViews/Buttons/DownloadView.swift
+++ Flight Tracer/Views/SubViews/Buttons/DownloadView.swift
@@ -23,11 +23,14 @@ struct DownloadView: View {
     }
     
     private func toArray(rowViewModels: [LogRowViewModel]) -> [[String]] {
-        let fieldNamesRow = LogMetadataLoader.getLogMetadata(named: "JeppesenLogFormat").map { $0.fieldName }
+        let logMetadata = LogMetadataLoader.getLogMetadata(named: "JeppesenLogFormat")
+        
+        let fieldNamesRow = logMetadata.flatMap { Array(repeating: $0.fieldName, count: $0.columnCount) }
+        
         let logRows = rowViewModels.map { $0.fields }
-    
+        
         print("logRows: \(logRows)")
-
+        
         return [fieldNamesRow] + logRows
     }
 }
